3e56cd00a2b3317215f2a334deb36c9370f5c504
Thomas Übellacker <thomas.uebellacker@prosiebensat1puls4.com>
2020-03-22 22:22:42 +0100

    add login via email

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -6,6 +6,8 @@ import Card from "@material-ui/core/Card"
 import CardActions from "@material-ui/core/CardActions"
 import CardContent from "@material-ui/core/CardContent"
 import Alert from "@material-ui/lab/Alert"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
 import {
   FacebookLoginButton,
   GoogleLoginButton
@@ -13,6 +15,8 @@ import {
 
 export default function() {
   const [errorAlert, setErrorAlert] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
     <div className={styles.root}>
       <Card>
@@ -23,6 +27,46 @@ export default function() {
           {errorAlert.length > 0 && (
             <Alert severity='error'>{errorAlert}</Alert>
           )}
+
+          <TextField
+            className={styles.textfield}
+            id='outlined-basic'
+            label='E-Mail'
+            variant='outlined'
+            type='email'
+            onChange={event => setEmail(event.target.value)}
+          />
+          <TextField
+            className={styles.textfield}
+            type='password'
+            id='outlined-basic'
+            label='Passwort'
+            variant='outlined'
+            onChange={event => setPassword(event.target.value)}
+          />
+          <Button
+            onClick={() => {
+              firebase
+                .auth()
+                .signInWithEmailAndPassword(email, password)
+                .catch(function(error) {
+                  setErrorAlert(error.message)
+                })
+            }}>
+            Login
+          </Button>
+          <Button
+            onClick={() => {
+              firebase
+                .auth()
+                .createUserWithEmailAndPassword(email, password)
+                .catch(function(error) {
+                  setErrorAlert(error.message)
+                })
+            }}>
+            Registrieren
+          </Button>
+          <hr className={styles.hr} />
           <FacebookLoginButton
             onClick={() => {
               const facebookAuthProvider = new firebase.auth.FacebookAuthProvider()
