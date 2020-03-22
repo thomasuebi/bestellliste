fc1adbb7d19022aee8ad87d421fd692f04248538
Thomas Übellacker <thomas.uebellacker@prosiebensat1puls4.com>
2020-03-22 13:29:53 +0100

    add error messages

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,16 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./LoginPage.module.css"
 import firebase from "firebase/app"
 import logo from "../HomePage/logo.png"
 import Card from "@material-ui/core/Card"
 import CardActions from "@material-ui/core/CardActions"
 import CardContent from "@material-ui/core/CardContent"
+import Alert from "@material-ui/lab/Alert"
 import {
   FacebookLoginButton,
   GoogleLoginButton
 } from "react-social-login-buttons"
 
 export default function() {
+  const [errorAlert, setErrorAlert] = useState("")
   return (
     <div className={styles.root}>
       <Card>
@@ -18,10 +20,19 @@ export default function() {
           <h1 className={styles.title}>
             <img src={logo} className={styles.logo} />
           </h1>
+          {errorAlert.length > 0 && (
+            <Alert severity='error'>{errorAlert}</Alert>
+          )}
           <FacebookLoginButton
             onClick={() => {
               const facebookAuthProvider = new firebase.auth.FacebookAuthProvider()
-              firebase.auth().signInWithPopup(facebookAuthProvider)
+              firebase
+                .auth()
+                .signInWithPopup(facebookAuthProvider)
+                .then(function(result) {})
+                .catch(function(error) {
+                  setErrorAlert(error.message)
+                })
             }}>
             <span>Weiter mit Facebook</span>
           </FacebookLoginButton>
