3bcd542824e74bc25bc06e973a36537789b5c4cf
thomasuebi <uebsi2804@gmail.com>
2021-11-17 00:54:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ import LoginPage from "./components/LoginPage/LoginPage"
 import HomePage from "./components/HomePage/HomePage"
 
 const config = {
-  apiKey: "AIzaSyBMRmd0qVwZlNKxddN71lpHAGgOMrt-7wc",
+  apiKey: "",
   authDomain: "bestellliste-e3334.firebaseapp.com",
   databaseURL: "https://bestellliste-e3334.firebaseio.com",
   projectId: "bestellliste-e3334",
   storageBucket: "bestellliste-e3334.appspot.com",
-  messagingSenderId: "702169543027",
-  appId: "1:702169543027:web:31061d919b597d2a42d506",
-  measurementId: "G-XD72ZYJ9DT"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 
 function App() {
